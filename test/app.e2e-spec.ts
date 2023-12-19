1c6d5621e100fdc2befa8db5e88d34a770afc1bd
devmonte <pacharaphon@adcm.co.th>
2023-12-19 10:43:19 +0700

    e2e test passed

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -253,23 +253,6 @@ describe('api-test e2e', () => {
 	});
 
 	describe('Seat', () => {
-		// describe('create an Event for the seat', () => {
-		// 	const eventDto: CreateEventDto = {
-		// 		name: 'New Year Concert',
-		// 		description:
-		// 			'The aniversary concert event at the end of the year.',
-		// 		datetimeStart: '2023-12-31 18:00:00',
-		// 		totalSeat: 300,
-		// 	};
-		// 	it('if valid input, should create Event', () => {
-		// 		return pactum
-		// 			.spec()
-		// 			.post('/events')
-		// 			.withBody(eventDto)
-		// 			.expectStatus(201)
-		// 			.stores('eventId', 'id');
-		// 	});
-		// });
 		describe('create Seat by Admin', () => {
 			const eventDto: CreateEventDto = {
 				name: 'New Year Concert',
@@ -307,73 +290,216 @@ describe('api-test e2e', () => {
 					.expectStatus(201)
 					.stores('seatId', 'id');
 			});
-		});
 
-		describe('Get all Seat', () => {
-			it('if db have the Seat, should get all event', () => {
-				return pactum
-					.spec()
-					.get('/seats')
-					.expectStatus(200)
-					.expectJsonLength(1);
+			describe('Get all Seat', () => {
+				it('if db have the Seat, should get all event', () => {
+					return pactum
+						.spec()
+						.get('/seats')
+						.expectStatus(200)
+						.expectJsonLength(1);
+				});
+			});
+
+			describe('Get Seat by id', () => {
+				it('if db have this Seat id, should get the Seat', () => {
+					return pactum
+						.spec()
+						.get('/seats/{id}')
+						.withPathParams('id', '$S{seatId}')
+						.expectStatus(200)
+						.expectBodyContains('$S{seatId}');
+				});
+			});
+
+			describe('Edit Seat by id', () => {
+				const seatDto: UpdateSeatDto = {
+					row: 10,
+					zone: 'Q',
+					eventId: 1,
+				};
+				const invalidSeatDto: UpdateSeatDto = {
+					row: 9,
+					zone: '10',
+				};
+				it('if input valid data, should edit seat success', () => {
+					return pactum
+						.spec()
+						.patch('/seats/{id}')
+						.withPathParams('id', '$S{seatId}')
+						.withJson({
+							row: 10,
+							zone: 'Q',
+							eventId: '$M{eventId2}',
+						})
+						.expectStatus(200)
+						.expectBodyContains(seatDto.row)
+						.expectBodyContains(seatDto.zone);
+				});
+				it('if input invalid data, should throw error', () => {
+					return pactum
+						.spec()
+						.patch('/seats/{id}')
+						.withPathParams('id', '$S{seatId}')
+						.withBody(invalidSeatDto)
+						.expectStatus(400);
+				});
+			});
+
+			describe('Delete Seat by id', () => {
+				it('if db have an Seat id, should delete Seat', () => {
+					return pactum
+						.spec()
+						.delete('/seats/{id}')
+						.withPathParams('id', '$S{seatId}')
+						.expectStatus(204);
+				});
+
+				it('after delete the last seat, should get empty array of seat', () => {
+					return pactum
+						.spec()
+						.get('/seats')
+						.expectStatus(200)
+						.expectJsonLength(0);
+				});
 			});
 		});
+	});
 
-		describe('Get Seat by id', () => {
-			it('if db have this Seat id, should get the Seat', () => {
-				return pactum
-					.spec()
-					.get('/seats/{id}')
-					.withPathParams('id', '$S{seatId}')
-					.expectStatus(200)
-					.expectBodyContains('$S{seatId}');
-			});
-		});
-
-		describe('Edit Seat by id', () => {
-			const seatDto: UpdateSeatDto = {
-				row: 10,
-				zone: 'Z',
+	describe('Reservation', () => {
+		describe('create Init Data for test reservation', () => {
+			const authDto: AuthDto = {
+				email: 'test_reserve@mail.com',
+				password: 'testtest',
+				name: 'testReserve',
+				phone: '0987654321',
 			};
-			const invalidSeatDto: UpdateSeatDto = {
-				row: 9,
-				zone: '10',
-			};
-			it('if input valid data, should edit seat success', () => {
-				return pactum
-					.spec()
-					.patch('/seats/{id}')
-					.withPathParams('id', '$S{seatId}')
-					.withBody(seatDto)
-					.expectStatus(200)
-					.expectBodyContains(seatDto.row)
-					.expectBodyContains(seatDto.zone);
-			});
-			it('if input invalid data, should throw error', () => {
-				return pactum
-					.spec()
-					.patch('/seats/{id}')
-					.withPathParams('id', '$S{seatId}')
-					.withBody(invalidSeatDto)
-					.expectStatus(400);
+			describe('Auth Register by User', () => {
+				it('body have all required data, should be pass and success', () => {
+					return pactum
+						.spec()
+						.post('/auth/register')
+						.withBody(authDto)
+						.expectStatus(201)
+						.stores('userAt', 'access_token');
+				});
 			});
 		});
+		const eventDto: CreateEventDto = {
+			name: 'Super Song Karn Concert',
+			description:
+				'The Most Famous Thai Festival and Bigest Concert in country.',
+			datetimeStart: '2024-04-13 18:00:00',
+			totalSeat: 400,
+		};
+		it('if valid input, should create reservation', async () => {
+			const res = await pactum
+				.spec()
+				.post('/events')
+				.withBody(eventDto)
+				.expectStatus(201);
 
-		describe('Delete Seat by id', () => {
-			it('if db have an Seat id, should delete Seat', () => {
-				return pactum
-					.spec()
-					.delete('/seats/{id}')
-					.withPathParams('id', '$S{seatId}')
-					.expectStatus(204);
+			stash.addDataMap({
+				eventId_reserve: res.json.id,
 			});
+			return res;
+		});
+		const seatDto: CreateSeatDto = {
+			row: 1,
+			zone: 'C',
+			eventId: parseInt(pactum.stash.getStoreKey('eventId_reserve')),
+		};
+		const seatDto2: CreateSeatDto = {
+			row: 2,
+			zone: 'D',
+			eventId: parseInt(pactum.stash.getStoreKey('eventId_reserve')),
+		};
+		it('if valid input, should create Seat for this Event', async () => {
+			const res = await pactum
+				.spec()
+				.post('/seats')
+				.withJson({
+					row: seatDto.row,
+					zone: seatDto.zone,
+					eventId: '$M{eventId_reserve}',
+				})
+				.expectStatus(201);
 
-			it('after delete the last seat, should get empty array of seat', () => {
+			stash.addDataMap({
+				seatId_reserve_1: res.json.id,
+			});
+			return res;
+		});
+
+		it('again, if valid input, should create Seat for this Event', async () => {
+			const res = await pactum
+				.spec()
+				.post('/seats')
+				.withJson({
+					row: seatDto2.row,
+					zone: seatDto2.zone,
+					eventId: '$M{eventId_reserve}',
+				})
+				.expectStatus(201);
+
+			stash.addDataMap({
+				seatId_reserve_2: res.json.id,
+			});
+			return res;
+		});
+
+		describe('Create and Get all Reserve', () => {
+			it('user do not have any reservation, should get empty array', () => {
 				return pactum
 					.spec()
-					.get('/seats')
+					.get('/reserve')
+					.withHeaders({
+						Authorization: 'Bearer $S{userAt}',
+					})
 					.expectStatus(200)
+					.expectBodyContains([])
 					.expectJsonLength(0);
+			});
+
+			it('if valid input, should create Reserve for the Event', () => {
+				return pactum
+					.spec()
+					.post('/reserve/events/{id}')
+					.withHeaders({
+						Authorization: 'Bearer $S{userAt}',
+					})
+					.withPathParams('id', '$M{eventId_reserve}')
+					.withBody({
+						seatId: '$M{seatId_reserve_1}',
+					})
+					.expectStatus(201)
+					.stores('reserveId', 'id');
+			});
+		});
+
+		describe('Get reserve by event', () => {
+			it('if user reserved the seat  in event, should event detail and seats', () => {
+				return pactum
+					.spec()
+					.get('/reserve/{id}')
+					.withPathParams('id', '$S{reserveId}')
+					.withHeaders({
+						Authorization: 'Bearer $S{userAt}',
+					})
+					.expectStatus(200);
+			});
+		});
+
+		describe('cancel reserve seat', () => {
+			it('if input valid data, should cancel reserve and release seat', () => {
+				return pactum
+					.spec()
+					.post('/reserve/{id}/cancel')
+					.withPathParams('id', '$S{reserveId}')
+					.withHeaders({
+						Authorization: 'Bearer $S{userAt}',
+					})
+					.expectStatus(201);
 			});
 		});
 	});
